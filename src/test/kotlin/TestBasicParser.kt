3483a1bc8016c9cf73cefdf484b2789c0851cfad
1287365321@qq.com <10839192+zzhaofisher@user.noreply.gitee.com>
2023-04-26 17:05:27 +0800

    test: test tokens to expression

--- src/test/kotlin/TestBasicParser.kt
+++ src/test/kotlin/TestBasicParser.kt
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.Test
+import tech.zzhdev.phunctions.expression.ConstantIntExpression
+import tech.zzhdev.phunctions.expression.OperatorExpression
+import tech.zzhdev.phunctions.expression.SymbolExpression
 import tech.zzhdev.phunctions.parser.IntToken
 import tech.zzhdev.phunctions.parser.OperatorTokens
 import tech.zzhdev.phunctions.parser.Parser
 import tech.zzhdev.phunctions.parser.Token
+import kotlin.test.AfterTest
 import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
-class TestStringToTokens {
+class TestBasicParser {
     @Test
     fun testStringToTokens() {
         val source = """
@@ -42,5 +47,39 @@ class TestStringToTokens {
             IntToken(4),
             OperatorTokens.RIGHT_PARENT
         ), tokens.toArray())
+    }
+
+    @Test
+    fun testTokensToExpression() {
+        val source = """
+            (*
+                (+ 1 1)
+                (* 2 2)
+                4
+            )
+        """.trimIndent()
+
+        val parser = Parser(source)
+        assert(parser.hasNext())
+
+        val expressionResult = parser.parse()
+        assert(expressionResult.isSuccess)
+
+        val expression = expressionResult.getOrNull()!!
+        val expected = SymbolExpression(arrayOf(
+            OperatorExpression("*"),
+            SymbolExpression(arrayOf(
+                OperatorExpression("+"),
+                ConstantIntExpression(1),
+                ConstantIntExpression(1),
+            )),
+            SymbolExpression(arrayOf(
+                OperatorExpression("*"),
+                ConstantIntExpression(2),
+                ConstantIntExpression(2),
+            )),
+            ConstantIntExpression(4),
+        ))
+        assertEquals(expected.toString(), expression.toString())
     }
 }
