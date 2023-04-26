72de5473f961078961ddb4e45e5e3945eef817e9
Zhihao <1287365321@qq.com>
2023-04-26 17:26:19 +0800

    feat: basic repl

--- src/main/kotlin/tech/zzhdev/phunctions/Main.kt
+++ src/main/kotlin/tech/zzhdev/phunctions/Main.kt
@@ -1,10 +1,31 @@
 package tech.zzhdev.phunctions
 
+import tech.zzhdev.phunctions.expression.Expression
 import tech.zzhdev.phunctions.expression.SymbolExpression
 import tech.zzhdev.phunctions.parser.Parser
+import java.util.*
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val source = """
+    if (args.contains("repl")) {
+        val scanner = Scanner(System.`in`)
+        print(">")
+        var value = scanner.nextLine()
+        while (value.trim() != "q") {
+            val p = Parser(value)
+            val expr: Expression = p.parse().getOrElse {
+                println(it)
+                exitProcess(-1)
+            }
+            println(expr.eval().getOrElse {
+                println(it)
+                exitProcess(-1)
+            })
+            print(">")
+            value = scanner.next()
+        }
+    } else {
+        val source = """
         (*
             (+ 1 1)
             (* 2 2)
@@ -13,8 +34,9 @@ fun main(args: Array<String>) {
         )
     """.trimIndent()
 
-    val parser = Parser(source)
-    val expression = parser.parse()
-    println(expression.getOrNull())
-    println(expression.getOrNull()?.eval())
+        val parser = Parser(source)
+        val expression = parser.parse()
+        println(expression.getOrNull())
+        println(expression.getOrNull()?.eval())
+    }
 }
